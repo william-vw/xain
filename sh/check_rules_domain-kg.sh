4c3f521a2ec48a39caea02883154bb328ff1fe6c
William Van Woensel <wvw@Williams-MacBook-Pro.local>
2023-05-18 13:55:53 -0400

    mixing up the 'gives' | improve print-check

--- sh/check_rules_domain-kg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kg=$1
-rules=$2
-
-eye --nope --n3 $rules $kg ../n3/modules/check/rules_domain-kg.n3 --query ../n3/modules/query_check.n3 > ../n3/tmp/final.n3
-# eye --nope --n3 $rules $kg ../n3/modules/check/rules_domain-kg.n3 --pass-only-new > ../n3/tmp/final.n3
