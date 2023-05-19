fc0ead853dab4fb7d4755ee92cf90213e6fcb819
William Van Woensel <wvw@Williams-MacBook-Pro.local>
2023-05-19 15:01:48 -0400

    fix rule inverting (avoid re-inverting same rule)

--- sh/contrast.sh
+++ sh/contrast.sh
@@ -3,7 +3,7 @@
 rules=$1
 goal=$2
 
-`eye --nope --n3 ../n3/modules/aux.n3 ../n3/modules/proof/contrast/invert_rules.n3 $rules --pass-only-new --quantify http://eyereasoner.github.io/var# > ../n3/tmp/rules-inv.n3`
+`eye --nope --n3 ../n3/modules/aux.n3 ../n3/modules/proof/contrast/invert_rules.n3 $rules --query ../n3/modules/proof/contrast/query_rules.n3 --quantify http://eyereasoner.github.io/var# > ../n3/tmp/rules-inv.n3`
 `eye --nope --n3 ../n3/modules/proof/query_heads.n3 ../n3/tmp/rules-inv.n3 --pass-only-new --quantify http://eyereasoner.github.io/var# > ../n3/tmp/rule-heads.n3`
 `eye --n3 $goal ../n3/tmp/rules-inv.n3 --query ../n3/tmp/rule-heads.n3 > ../n3/tmp/proof.n3`
 `eye --nope --n3 ../n3/tmp/proof.n3 ../n3/modules/proof/contrast/aux_inv.n3  --query ../n3/modules/proof/query_proof.n3 > ../n3/tmp/proof-annotated.n3`
